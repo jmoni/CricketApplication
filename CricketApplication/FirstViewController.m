811e442926382c81864c5750d17cafd8ed6e6d1e
Miranda Aperghis <mza05@hotmail.co.uk>
2012-07-30 18:25:56 +0100

    Miranda Second Commit

--- CricketApplication/FirstViewController.m
+++ CricketApplication/FirstViewController.m
@@ -13,6 +13,71 @@
 @end
 
 @implementation FirstViewController
+@synthesize homeTeamEntered = _homeTeamEntered;
+@synthesize awayTeamEntered = _awayTeamEntered;
+@synthesize dateButton = _dateButton;
+@synthesize dateText = _dateText;
+
+#define kDatePickerTag 100
+
+
+-(IBAction)showActionSheet:(id)sender {
+	UIActionSheet *popUpDate = [[UIActionSheet alloc] initWithTitle:@"Set the match date" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Select", nil];
+	[popUpDate showInView:self.view];
+    [popUpDate setFrame:CGRectMake(0, 117, 320, 383)];
+}
+
+- (void)willPresentActionSheetUIActionSheet: (UIActionSheet *)actionSheet {
+    
+    UIDatePicker *pickerView = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 40, 320, 216)];
+    
+    //Configure picker...
+    [pickerView setMinuteInterval:5];
+    [pickerView setTag: kDatePickerTag];
+    
+    //Add picker to action sheet
+    [actionSheet addSubview:pickerView];
+    
+    //Gets an array af all of the subviews of our actionSheet
+    NSArray *subviews = [actionSheet subviews];
+    
+    [[subviews objectAtIndex:1] setFrame:CGRectMake(20, 266, 280, 46)];
+    [[subviews objectAtIndex:2] setFrame:CGRectMake(20, 317, 280, 46)];
+    
+}
+
+- (void)actionSheet: (UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex != [actionSheet cancelButtonIndex]) {
+        
+        //set Date formatter
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"DD/MM/YYYY"];
+        
+        //Gets our picker
+        UIDatePicker *ourDatePicker = (UIDatePicker *) [actionSheet viewWithTag:kDatePickerTag];
+        
+        NSDate *selectedDate = [ourDatePicker date];
+        
+        _dateText.self.text = [formatter stringFromDate:selectedDate];
+        
+        NSString *msg = [[NSString alloc] initWithFormat:@"The date that you had selected was, %@", [formatter stringFromDate:selectedDate]];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Date" message:msg delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"First", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
@@ -22,13 +87,18 @@
 
 - (void)viewDidUnload
 {
+    [self setHomeTeamEntered:nil];
+    [self setAwayTeamEntered:nil];
+    [self setDateButton:nil];
+    [self setDateText:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 @end
+
