591b3d1fa377754b6f176efd3ceb3dd60ba3c53b
Tim Lisle <tim.lisle92@gmail.com>
2012-07-31 12:40:48 +0100

    Third Tab started

---
+++ CricketApplication/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  CricketApplication
+//
+//  Created by Lion User on 31/07/2012.
+//  Copyright (c) 2012 JMoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ CricketApplication/ThirdViewController.m
@@ -0,0 +1,43 @@
+//
+//  ThirdViewController.m
+//  CricketApplication
+//
+//  Created by Lion User on 31/07/2012.
+//  Copyright (c) 2012 JMoni. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
