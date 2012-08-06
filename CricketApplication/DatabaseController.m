be3c255129a2c58ffa140f1a2b7497500b2fa2b7
Miranda Aperghis <mza05@hotmail.co.uk>
2012-08-06 15:42:16 +0100

    Database Controller added

---
+++ CricketApplication/DatabaseController.m
@@ -0,0 +1,46 @@
+//
+//  DatabaseController.m
+//  CricketApplication
+//
+//  Created by Miranda Aperghis on 06/08/2012.
+//  Copyright (c) 2012 JMoni. All rights reserved.
+//
+
+#import "DatabaseController.h"
+
+
+@interface DatabaseController ()
+
+@end
+
+@implementation DatabaseController
+@synthesize saveButton = _saveButton;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [self setSaveButton:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
