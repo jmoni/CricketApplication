3653532f46b0b8f4d3c248f35b9ba94e16ea56ea
Rob <rob-lloyd@hotmail.co.uk>
2012-08-07 09:55:07 +0100

    Database is added t =o correct pace if one doesn't already existe

--- CricketApplication/DatabaseController.m
+++ CricketApplication/DatabaseController.m
@@ -13,45 +13,60 @@
 #include "FirstViewController.h"
 
 @interface DatabaseController ()
-
 @end
+
+const char *dbpath;
 
 @implementation DatabaseController
 @synthesize saveButton = _saveButton;
 
-
-- (void) saveData : (id) sender
-{
-    NSString *docsDir;
-     NSArray *dirPaths;
-     
-     // Get the documents directory
-     dirPaths = NSSearchPathForDirectoriesInDomains(
-     NSDocumentDirectory, NSUserDomainMask, YES);
-     
-     docsDir = [dirPaths objectAtIndex:0];
-    NSLog(@"%@", docsDir);
-    sqlite3_stmt    *statement;
-    databasePath = [[NSString alloc] initWithString: @"/Users/miranda/Developer/CricketApplication/CricketApplication/cricket.db"];
-    const char *dbpath = [databasePath UTF8String];
-    
-    if (sqlite3_open(dbpath, &cricketDB) == SQLITE_OK)
-    {
-        NSString *insertSQL = [NSString stringWithFormat: @"INSERT INTO TEAMS (TeamID, TeamName) VALUES (20, \"Hello\")"];
-      const char *insert_stmt = [insertSQL UTF8String];
-      sqlite3_prepare_v2(cricketDB, insert_stmt,-1, &statement, NULL);
-      if (sqlite3_step(statement) == SQLITE_DONE)
-      {
-          NSLog(@"Adding worked");
-      } else {
-          NSLog(@"Adding failed");
-      }
-      sqlite3_finalize(statement);
-      sqlite3_close(cricketDB);
-      }
+// Creates a writable copy of the bundled default database in the application Documents directory.
+- (void)createEditableCopyOfDatabaseIfNeeded {
+    // First, test for existence.
+    BOOL success;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *writableDBPath = [documentsDirectory stringByAppendingPathComponent:@"cricket.db"];
+	//NSLog(@"Accessed DB at %@", writableDBPath);
+	dbpath = [writableDBPath UTF8String];
+    success = [fileManager fileExistsAtPath:writableDBPath];
+    if (success){
+        return;
+	}
+	//NSLog(@"Accessed DB at %@", writableDBPath);
+    // The writable database does not exist, so copy the default to the appropriate location.
+    NSString *defaultDBPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"cricket.db"];
+    success = [fileManager copyItemAtPath:defaultDBPath toPath:writableDBPath error:&error];
+    if (!success) {
+        NSAssert1(0, @"Failed to create writable database file with message '%@'.", [error localizedDescription]);
+    }
 }
 
+- (void) saveData:(id)sender{
+    //[self insertStringIntoDatabase:[NSString stringWithFormat: @"INSERT INTO TEAMS VALUES (null, \"HELLOWORLD\")"]];
+}
 
+- (void)insertStringIntoDatabase:(NSString *)string {
+	/*sqlite3_stmt *statement;
+    if (sqlite3_open(dbpath, &cricketDB) == SQLITE_OK)
+    {
+		NSLog(@"Accessed DB");
+		NSString *insertSQL = string;
+		const char *insert_stmt = [insertSQL UTF8String];
+		sqlite3_prepare_v2(cricketDB, insert_stmt,-1, &statement, NULL);
+		if (sqlite3_step(statement) == SQLITE_DONE) {
+			NSLog(@"Access worked");
+		} else {
+			NSLog(@"Access failed");
+		}
+		sqlite3_finalize(statement);
+		sqlite3_close(cricketDB);
+	} else {
+		NSLog(@"Could not access DB");
+	}*/
+}
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -64,43 +79,7 @@
 }
 
 - (void)viewDidLoad {
-    /*NSString *docsDir;
-    NSArray *dirPaths;
-    
-    // Get the documents directory
-    dirPaths = NSSearchPathForDirectoriesInDomains(
-                                                   NSDocumentDirectory, NSUserDomainMask, YES);
-    
-    docsDir = [dirPaths objectAtIndex:0];
-    
-    // Build the path to the database file
-    databasePath = [[NSString alloc]
-                    initWithString: [docsDir stringByAppendingPathComponent:
-                                     @"cricket.db"]];
-    
-    NSFileManager *filemgr = [NSFileManager defaultManager];
-    
-    if ([filemgr fileExistsAtPath: databasePath ] == NO)
-    {
-        const char *dbpath = [databasePath UTF8String];
-        
-        if (sqlite3_open(dbpath, &contactDB) == SQLITE_OK)
-        {
-            char *errMsg;
-            
-            const char *sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)";
-            
-            if (sqlite3_exec(contactDB, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK)
-            {
-                status.text = @"Failed to create table";
-            }
-            
-            sqlite3_close(contactDB);
-            
-        } else {
-            status.text = @"Failed to open/create database";
-        }
-    }*/
+    [self createEditableCopyOfDatabaseIfNeeded];
     [super viewDidLoad];
 }
 
