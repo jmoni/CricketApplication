49e4a27984de2f4a5f9717573e50b5fbf84cca08
Rob <rob-lloyd@hotmail.co.uk>
2012-08-15 12:25:17 +0100

    share kit added and configured

---
+++ CricketApplication/MySHKConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  MySHKConfiguration.h
+//  CricketApplication
+//
+//  Created by Rob Lloyd on 15/08/2012.
+//  Copyright (c) 2012 JMoni. All rights reserved.
+//
+
+#import "DefaultSHKConfigurator.h"
+
+@interface MySHKConfiguration : DefaultSHKConfigurator
+
+@end
